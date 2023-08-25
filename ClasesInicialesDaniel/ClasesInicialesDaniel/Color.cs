f6439a014a5fe910166b61001b8ad6ee017bfcfe
danielgortz <142053799+danielgortz@users.noreply.github.com>
2023-08-24 23:03:08 -0600

    smarphone

--- ClasesInicialesDaniel/ClasesInicialesDaniel/Color.cs
+++ ClasesInicialesDaniel/ClasesInicialesDaniel/Color.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ClasesInicialesDaniel
+namespace ClasessIniciales_002
 {
    public class Color
     {
--- ClasesInicialesDaniel/ClasesInicialesDaniel/Program.cs
+++ ClasesInicialesDaniel/ClasesInicialesDaniel/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ClasesInicialesDaniel
+namespace ClasessIniciales_002
 {
     class Program
     {
@@ -136,6 +136,6 @@ namespace ClasesInicialesDaniel
     }
 }
 
-        }
-    }
-}
+        
+   
+
