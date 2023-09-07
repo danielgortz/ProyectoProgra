09ebd91ceab81cb3c70a349b7afa24930d932da2
danielgortz <142053799+danielgortz@users.noreply.github.com>
2023-09-07 14:52:32 -0600

    generador

---
+++ ClasesInicialesDaniel/Generador de Nombres/generadordevillanos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generador_de_Nombres
+{
+    class generadordevillanos
+    {
+        static void Main(string[] args);
+         
+        string[] NomVillanos= {"oscuro"}
+
+
+    }
+}
