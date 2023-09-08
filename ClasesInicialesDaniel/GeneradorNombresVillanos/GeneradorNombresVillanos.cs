6e7c157496d5480f26d29f5e6e36e704374135fa
danielgortz <dniel.go11@gmail.com>
2023-09-08 13:13:25 -0600

    ejerccio3

---
+++ ClasesInicialesDaniel/GeneradorNombresVillanos/GeneradorNombresVillanos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneradorNombresVillanos
+{
+    public class GeneradorNombresVillanos
+    {
+             //Enero 1           //Febrero 2      //Marzo 3
+
+
+    }
+}
