11285471ddafdf30f5f095aa713e38e4bc29dfd8
danielgortz <142053799+danielgortz@users.noreply.github.com>
2023-10-20 00:42:45 -0600

    daño cavernicola

--- Myproyect/Assets/Sctipts/Personaje.cs
+++ Myproyect/Assets/Sctipts/Personaje.cs
@@ -8,15 +8,21 @@ public class Personaje : MonoBehaviour
     public int hpMax = 100;
     public int score = 0;
     public int vidas = 3;
-    
+    private Animator miAnimador;
+    void Start()
+    {
+        miAnimador = GetComponent<Animator>();
+    }
     public void hacerDanio(int puntos, GameObject atacante)
     {
-        print(name + "recibe daño de"
-            + puntos + "por" + atacante.name);
-
-        //resto los puntos al HP actual
+        print(name + "recibe daño de " + puntos + "por" + atacante.name);
         hp = hp - puntos;
-      
- 
+        miAnimador.SetTrigger("DAÑAR");
+    }
+    public void perderVida(int puntosVida, GameObject atacante)
+    {
+        print(name + "Muere por " + atacante.name);
+        vidas = vidas - puntosVida;
+       hp=0;
     }
 }
